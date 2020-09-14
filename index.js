0e8b39709faa88ae35f43b4a18530be385a0d8ca
SiddhantJangir <71001670+SiddhantJangir@users.noreply.github.com>
2020-09-14 10:12:35 +0530

    <myName>

---
+++ index.js
@@ -0,0 +1,40 @@
+// var a = "Hello, Dowls!";
+// document.write(a);
+
+/*
+What are variables?
+    Variables are containers to store some values Like 1234 or "Hello, World!"
+
+*/
+
+//Starting Variables from here:
+
+
+
+
+
+
+
+
+
+
+//DataTypes:
+/**
+ * What are DataTypes?
+ *      Datatypes are types which tell a container to what to store in it like we
+ *      use a bucket to store water or paint or any liquid material
+ * 
+ *      Exectly like that we use datatypes to tell the container(Variables) to store what in it
+ *      like it can be a number or a string(a set of characters of english) etc
+ * 
+ *      some of the datatypes are :
+ * 
+ *          1.String:
+ *              A String is a Set of Characters
+ * 
+ *          2.Int       
+ *              An int is an integer/set of integers
+ */
+
+
+ //Starting DataTypes from here:
